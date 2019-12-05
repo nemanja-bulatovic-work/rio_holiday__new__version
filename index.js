7d3bab35a5001a6a3cc34ef6bc36a0ab4b8d8df5
User <user@Users-MacBook-Pro.local>
2019-12-05 18:20:14 +0100

    third

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var PlxItems = {};
 
 // config params that need to be aligned with css
-var Config = {
+var ConfigDesk = {
     // bottom css attribute (px)
     HEAD_START: 5,
     HEAD_TOP_OFFSET: 0,
@@ -13,20 +13,55 @@ var Config = {
     NAV_OFFSET: -500,
     // translate attribute
     IMG_1_START: 0,
-    IMG_1_MAX_OFFSET: -100,
+    IMG_1_MAX_OFFSET: -3.5,
     // translate attribute
     IMG_2_START: 0,
-    IMG_2_MAX_OFFSET: -100,
+    IMG_2_MAX_OFFSET: -5,
     // translate attribute
     IMG_3_START: 0,
-    IMG_3_MAX_OFFSET: -400,
+    IMG_3_MAX_OFFSET: -15,
+    // translate attribute
+    IMG_4_START: -10,
+    IMG_4_MAX_OFFSET: -33,
+    // margin-top attribute (in vw)
+    IMG_5_START: 30,
+    IMG_5_MAX_OFFSET: -55,
+};
+
+var ConfigMob = {
+    // bottom css attribute (px)
+    HEAD_START: 5,
+    HEAD_TOP_OFFSET: 0,
+    //second-page start
+    PAGE_START: 0,
+    PAGE_OFFSET: -300 ,
+    // translate attribute
+    NAV_START: 0,
+    NAV_OFFSET: -500,
+    // translate attribute
+    IMG_1_START: 0,
+    IMG_1_MAX_OFFSET: -33.5,
+    // translate attribute
+    IMG_2_START: 0,
+    IMG_2_MAX_OFFSET: -45,
+    // translate attribute
+    IMG_3_START: 0,
+    IMG_3_MAX_OFFSET: -65,
     // translate attribute
     IMG_4_START: 0,
-    IMG_4_MAX_OFFSET: -700,
+    IMG_4_MAX_OFFSET: -103,
     // margin-top attribute (in vw)
-    IMG_5_START: -900,
-    IMG_5_MAX_OFFSET: 0,
+    IMG_5_START: 30,
+    IMG_5_MAX_OFFSET: -165,
 };
+
+var Config = {}
+
+if (window.matchMedia("(max-width:768px)").matches){
+    Config = ConfigMob
+}else{
+    Config = ConfigDesk
+}
 
 function getMaxScroll() {
     return Math.max(document.body.scrollHeight, document.body.offsetHeight, 
@@ -56,24 +91,24 @@ function setPlxItems() {
             setter: (elem, value) => elem.style.transform = `translateY(-${ value }px)`,
         },
         img1: {
-            element: document.querySelector(".background_2"),
-            setter: (elem, value) => elem.style.transform = `translateY(-${ value }px)`,
+            element: document.querySelector(".background_1"),
+            setter: (elem, value) => elem.style.transform = `translateY(-${ value }vw)`,
         },
         img2: {
             element: document.querySelector(".background_2"),
-            setter: (elem, value) => elem.style.transform = `translateY(-${ value }px)`,
+            setter: (elem, value) => elem.style.transform = `translateY(-${ value }vw)`,
         },
         img3: {
             element: document.querySelector(".background_3"),
-            setter: (elem, value) => elem.style.transform = `translateY(-${ value }px)`,
+            setter: (elem, value) => elem.style.transform = `translateY(-${ value }vw)`,
         },
         img4: {
             element: document.querySelector(".background_4"),
-            setter: (elem, value) => elem.style.transform = `translateY(-${ value }px)`,
+            setter: (elem, value) => elem.style.transform = `translateY(-${ value }vw)`,
         },
         img5: {
             element: document.querySelector(".background_5"),
-            setter: (elem, value) => elem.style.marginTop = `-${ value }px`,
+            setter: (elem, value) => elem.style.marginTop = `-${ value }vw`,
         },
     }
 }
@@ -115,10 +150,12 @@ const SET = (plxItem, value) => {
 }
 
 document.addEventListener("scroll", function(e) {
-    if(document.querySelector('.first__page').offsetHeight === window.scrollY){
+    if(document.querySelector('.first__page').offsetHeight <= window.scrollY){
         document.querySelector('.second__page').style.overflowY = "scroll"
+        document.querySelector('.first__page').style.opacity = 0;
     }else{
         document.querySelector('.second__page').style.overflowY = "hidden"
+        document.querySelector('.first__page').style.opacity = 1;
     }
     const MAX_SCROLL = getMaxScroll();
     const plxFactor =  window.scrollY / MAX_SCROLL;
@@ -130,29 +167,29 @@ document.addEventListener("scroll", function(e) {
 
 setTimeout(function(){
     document.getElementById('bg-change').setAttribute("class", "first__page sepia-color");
-},100);
+},1000);
 
 setTimeout(function(){
     document.getElementById('secondwordnav').setAttribute("class", "second active-2");
-},300);
+},3000);
 
 setTimeout(function(){
     document.getElementById('thirdwordnav').setAttribute("class", "third active-2");
-},310);
+},3100);
 
 setTimeout(function(){
     document.getElementById('firstwordnav').setAttribute("class", "first active");
-},550);
+},5500);
 
 setTimeout(function(){
     document.getElementById("first-rio").setAttribute("class", "page__head__text active");
-},560);
+},5600);
 
 setTimeout(function(){
     document.getElementById("bgimages").setAttribute("class", "background active-3");
     document.body.style.overflowY = "scroll";
     setPlxItems();
-}, 570);
+}, 5700);
 
 
 
